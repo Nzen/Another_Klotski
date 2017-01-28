53a3ea63d4c6740d2ec708339d40763ffb99e845
Nicholas Prado <nmprado@nzen.ws>
2017-01-28 14:43:07 -0800

    checkin for diff venue, still not working

--- web/klientski.js
+++ web/klientski.js
@@ -4,105 +4,75 @@
 Graphics client that draws a Klotski game
 */
 
-// page utilities
 
-window.addEventListener( "load", pageReady, false );
-
-var termin = function() {};
+const termin = function() {};
 termin.log = function( message )
 {
 	try
-	{ console.log( message ); }
-	catch ( exception )
-	{ return; } // IE reputedly has no console.
-}
-
-
-function canvasSupported() // doesn't catch all mobile exceptions
-{
-	return ! window.CanvasRenderingContext2D;
-}
-
-
-function socketSupported()
-{
-	return "WebSocket" in window;
-}
-
-
-function pageReady()
-{
-	if ( ! ( canvasSupported() || socketSupported() ) )
 	{
-		termin.log( "prerequisites unsupported, sorree" );
-		return;
+		console.log( message );
 	}
-	else
-		klotski();
+	catch ( exception )
+	{
+		return;
+	} // IE reputedly has no console.
 }
 
-
-function klotski()
-{
-	termin.log( "Change to real" ); // FIX
-}
 
 // drawing
 
+
+	const background = "#0A0A29";
+
 function drawTesting( letter )
 {
-	var dom_canvas = document.getElementById( "canvas_here" );
-	var canv = dom_canvas.getContext( "2d" );
+	const dom_canvas = document.getElementById( "canvas_here" );
+	const canv = dom_canvas.getContext( "2d" );
 	canv.fillStyle = "gray";
-	canv.font = "bold 13px monospace";
-	canv.fillText( letter, 15, 15 );
+	canv.font = "bold 25px monospace";
+	canv.fillText( letter, 30, 30 );
 }
 
 
 // input
 
-	function letter_pressed( ev ) // FIX
+function KlInput()
+{}
+
+KlInput.prototype =
+{
+
+	isInterestingInput : function( letter )
+	{
+		return isMovementInput( letter ) || isUtilityInput( letter );
+	},
+
+
+	isMovementInput : function ( letter )
+	{
+		return "wasdWASD".indexOf( letter ) >= 0;
+	},
+
+
+	isUtilityInput : function( letter )
+	{
+		return "~%".indexOf( letter ) >= 0;
+	},
+
+
+	letterPressed : function ( ev ) // FIX
 	{
 		var keyPressed = String.fromCharCode( ev.keyCode );
-		drawTesting( keyPressed ); // 4TESTS
-		switch( keyPressed )
+		if ( interestingInput( keyPressed ) )
 		{
-		// cursor & shape
-		case 'A':
-			termin.log( "bound.try_block( ky.L ");
-			break;
-		case 'W':
-			termin.log( "bound.try_block( ky.U ");
-			break;
-		case 'D':
-			termin.log( "bound.try_block( ky.R ");
-			break;
-		case 'S':
-			termin.log( "bound.try_block( ky.D ");
-			break;
-		// cursor only
-		case 'a':
-			termin.log( "bound.try_cursor( ky.l ");
-			break;
-		case 'w':
-			termin.log( "bound.try_cursor( ky.u ");
-			break;
-		case 'd':
-			termin.log( "bound.try_cursor( ky.r ");
-			break;
-		case 's':
-			termin.log( "bound.try_cursor( ky.d ");
-			break;
-		case '~':
-			termin.log( "save_game("); // make it work, then make it good.
-			break;
-		case '%':
-			 termin.log( "restore_game(");
-			break;
-		default:
-			return;
+			drawTesting( keyPressed ); // 4TESTS
+		}
+		else
+		{
+			termin.log( "not interested in "+ keyPressed );
 		}
 	}
+};
 
 
 // networking
@@ -147,11 +117,43 @@ error
 */
 
 
-// game state and loop
+// page utilities
 
-	var background = "#0A0A29";
+function canvasSupported() // doesn't catch all mobile exceptions
+{
+	return ! window.CanvasRenderingContext2D;
+}
 
-window.addEventListener( "keypress", letter_pressed, true ); // won't work with IE < 9, but neither will canvas
+
+function socketSupported()
+{
+	return "WebSocket" in window;
+}
+
+
+// var channel;
+
+
+function pageReady()
+{
+	if ( ! ( canvasSupported() || socketSupported() ) )
+	{
+		termin.log( "prerequisites unsupported, sorree" );
+		return;
+	}
+	else
+	{
+		var listensForKlotski = KlInput();
+		termin.log( listensForKlotski.isInterestingInput( 'f' ) );
+		window.addEventListener( "keypress", listensForKlotski.letterPressed, true );
+		 	// NOTE won't work with IE < 9, but neither will canvas
+		// channel = new WebSocket( "ws://localhost:9998" );
+	}
+}
+
+
+window.addEventListener( "load", pageReady, false );
+
 
 
 
